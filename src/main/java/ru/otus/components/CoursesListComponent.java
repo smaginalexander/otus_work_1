e5f9db4f67d05b61519f2a0fe0ce45693bc9d108
Alexander <smaginalexander@yandex.ru>
2023-03-05 13:08:52 +0300

    test

--- src/main/java/ru/otus/components/CoursesListComponent.java
+++ src/main/java/ru/otus/components/CoursesListComponent.java
@@ -20,68 +20,68 @@ public class CoursesListComponent extends AbstractComponent<CoursesListComponent
     public CoursesListComponent(WebDriver driver) {
         super(driver);
     }
-
-    public void clickOnTheNextCourse() {
-        deletePopup();
-        for (CourseData courseData : CourseData.values()) {
-            if (courseData.getDate().equals(filterNextDateOfCourses())) {
-                WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, courseData.getCategory(), courseData.getName())));
-                System.out.println("ближайший курс " + courseData.getName() + "стартует " + courseData.getDate());
-                cours.click();
-                return;
-            }
-        }
-
-    }
-
-    public void clickOnTheLastCourse() {
-        deletePopup();
-        for (CourseData courseData : CourseData.values()) {
-            if (courseData.getDate().equals(filterLastDateOfCourses())) {
-                WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, courseData.getCategory(), courseData.getName())));
-                System.out.println("Последний курс " + courseData.getName() + "стартует " + courseData.getDate());
-                cours.click();
-            }
-        }
-    }
-
-    public String filterNextDateOfCourses() {
-        Optional<LocalDate> theNextDateFilter = getListOfDatesCourses().stream().filter(date -> date.isAfter(LocalDate.now())).
-        reduce((maxDate, curDate) -> maxDate.isBefore(curDate) ?
-                LocalDate.parse(maxDate.toString()) : LocalDate.parse(curDate.toString()));
-        String theNextDate = theNextDateFilter.stream().findFirst().get().toString();
-        return theNextDate;
-    }
-
-    public String filterLastDateOfCourses() {
-        Optional<LocalDate> theLastDateFilter = getListOfDatesCourses().stream().
-                reduce((maxDate, curDate) -> maxDate.isAfter(curDate) ?
-                        LocalDate.parse(maxDate.toString()) : LocalDate.parse(curDate.toString()));
-        String theLustDate = theLastDateFilter.stream().findFirst().get().toString();
-        return theLustDate;
-    }
-
-    //Дублирующая часть кода ванесена в отельный метод
-    public List<LocalDate> getListOfDatesCourses() {
-        List<LocalDate> dates = new ArrayList<>();
-        for (CourseData courseData : CourseData.values()) {
-            dates.add(LocalDate.parse(courseData.getDate()));
-        }
-        return dates;
-    }
-
-    public void deletePopup() {
-        actions.pause(3000).perform();
-        WebElement popupCookie = driver.findElement(By.xpath("//div[contains(@class, 'cookies__margin-block')]"));
-        WebElement helpPopup = driver.findElement(By.xpath("//jdiv[@id='jvlabelWrap']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", popupCookie);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", helpPopup);
-    }
-
-    public void clickOnCours(String category, String coursName) {
-        deletePopup();
-        WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, category, coursName)));
-        cours.click();
-    }
+//
+//    public void clickOnTheNextCourse() {
+//        deletePopup();
+//        for (CourseData courseData : CourseData.values()) {
+//            if (courseData.getDate().equals(filterNextDateOfCourses())) {
+//                WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, courseData.getCategory(), courseData.getName())));
+//                System.out.println("ближайший курс " + courseData.getName() + "стартует " + courseData.getDate());
+//                cours.click();
+//                return;
+//            }
+//        }
+//
+//    }
+//
+//    public void clickOnTheLastCourse() {
+//        deletePopup();
+//        for (CourseData courseData : CourseData.values()) {
+//            if (courseData.getDate().equals(filterLastDateOfCourses())) {
+//                WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, courseData.getCategory(), courseData.getName())));
+//                System.out.println("Последний курс " + courseData.getName() + "стартует " + courseData.getDate());
+//                cours.click();
+//            }
+//        }
+//    }
+//    Optional<LocalDate> theNextDateFilter = getListOfDatesCourses().stream().filter(date -> date.isAfter(LocalDate.now())).
+//            reduce((maxDate, curDate) -> maxDate.isBefore(curDate) ?
+//                    LocalDate.parse(maxDate.toString()) : LocalDate.parse(curDate.toString()));
+//    String theNextDate = theNextDateFilter.stream().findFirst().get().toString();
+//        return theNextDate;
+//}
+//
+//    public String filterLastDateOfCourses() {
+//        Optional<LocalDate> theLastDateFilter = getListOfDatesCourses().stream().
+//                reduce((maxDate, curDate) -> maxDate.isAfter(curDate) ?
+//                        LocalDate.parse(maxDate.toString()) : LocalDate.parse(curDate.toString()));
+//        String theLustDate = theLastDateFilter.stream().findFirst().get().toString();
+//        return theLustDate;
+//    }
+//
+//    //Дублирующая часть кода ванесена в отельный метод
+//    public List<LocalDate> getListOfDatesCourses() {
+//        List<LocalDate> dates = new ArrayList<>();
+//        for (CourseData courseData : CourseData.values()) {
+//            dates.add(LocalDate.parse(courseData.getDate()));
+//        }
+//        return dates;
+//    }
+//    public String filterNextDateOfCourses() {
+//
+//
+//    public void deletePopup() {
+//        actions.pause(3000).perform();
+//        WebElement popupCookie = driver.findElement(By.xpath("//div[contains(@class, 'cookies__margin-block')]"));
+//        WebElement helpPopup = driver.findElement(By.xpath("//jdiv[@id='jvlabelWrap']"));
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", popupCookie);
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", helpPopup);
+//    }
+//
+//    public void clickOnCours(String category, String coursName) {
+//        deletePopup();
+//        WebElement cours = driver.findElement(By.xpath(String.format(coursXpath, category, coursName)));
+//        cours.click();
+//    }
 
 }
